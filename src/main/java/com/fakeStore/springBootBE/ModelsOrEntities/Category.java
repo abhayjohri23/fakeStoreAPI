e2dab9e653fdbec0a8209b97ea92a7a77693e347
abhayjohri23 <abhay.johri0@gmail.com>
2024-01-07 22:16:36 +0530

    Lecture 2: Added Inheritance relation between product and category with cardinality relation b/w two.MappedBy and JoinColumn is used

--- src/main/java/com/fakeStore/springBootBE/ModelsOrEntities/Category.java
+++ src/main/java/com/fakeStore/springBootBE/ModelsOrEntities/Category.java
@@ -1,11 +1,26 @@
 package com.fakeStore.springBootBE.ModelsOrEntities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-@Entity
+
+import java.util.List;
+
+@Entity(name = "category")
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category extends BaseModel{
     private String categoryName;
+
+    @OneToMany(mappedBy = "category")
+    /*mappedBy - Used for the indication of the inverse or the referencing side simply should map to the owning side (i.e. Product).
+    * The value of mappedBy is the name of the association-mapping attribute (=Category class's category attribute) on the owning side. */
+    private List<Product> productList;
+
+    //Follow: https://www.baeldung.com/jpa-joincolumn-vs-mappedby
 }
--- src/main/java/com/fakeStore/springBootBE/ModelsOrEntities/Product.java
+++ src/main/java/com/fakeStore/springBootBE/ModelsOrEntities/Product.java
@@ -1,10 +1,11 @@
 package com.fakeStore.springBootBE.ModelsOrEntities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
-@Entity
+@Entity(name = "Product")
 @Getter
 @Setter
 public class Product extends BaseModel{
@@ -13,8 +14,15 @@ public class Product extends BaseModel{
     private String description;
     private double price;
     @ManyToOne
+    @JoinColumn(name = "categoryId")        //Mind we have given the name to the column which has FK
     private Category category;
+    //It means that we have configured inheritance and association of category and product class.
+    //@JoinColumn refers to the foreign key taken from the associated table (i.e. Product). We have given its name by the attribute "name"
+    //@JoinColumn() is always used on the owner side of the relation. Owner = "Entity contained within another Entity". Here Product is contained within Category.
+    //So, product side is assigned the ownership. (Reverse is also possible but not recommended)
 
+    /*OneToMany and ManyToOne are applied together for bidirectional nature of the relation, which means that both the entity details can be retrieved from each other's table.*/
+    //It is recommended to use both together, although one can be used individually as well.
     public String toString(){
         return "{"+title+","+description+","+price+","+category+","+image+"}";
     }
